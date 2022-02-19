20f017ae7241dc56d6146a5fa361d7619ee9e727
michaelyang02 <michael626yang@gmail.com>
2022-02-19 21:43:51 +0000

    Change error handling when processing payment

--- handlers/general_handler_error.py
+++ handlers/general_handler_error.py
@@ -4,19 +4,28 @@ class GeneralHandlerError(Exception):
         self.function = function
         self.request_dict = request_dict
 
+# The database has suffered internal errors or exceptions such that the transaction has failed
 class DatabaseTransactionError(GeneralHandlerError):
     pass
 
+# There is insufficient fund in the cash account to carry out the transaction
 class InsufficientFundError(GeneralHandlerError):
     pass
 
+# Attempting to create another footprint or offset account
 class MoreThanOneFootprintOrOffsetAccountError(GeneralHandlerError):
     pass
 
+# Attempting to refine a non-footprint transaction
 class NotFootprintTransactionRefinedError(GeneralHandlerError):
     pass
 
+# The timestamp passed is invalid
 class InvalidTimestampError(GeneralHandlerError):
+    pass
+
+# There is no associated footprint account
+class NoAssociatedFootprintAccountError(GeneralHandlerError):
     pass
 
 STATUS_CODE_ERROR_DICTIONARY = {
@@ -25,6 +34,7 @@ STATUS_CODE_ERROR_DICTIONARY = {
     3 : MoreThanOneFootprintOrOffsetAccountError,
     4 : NotFootprintTransactionRefinedError,
     5 : InvalidTimestampError,
+    6 : NoAssociatedFootprintAccountError
 }
 
 ERROR_STATUS_CODE_DICTIONARY = {v: k for k, v in STATUS_CODE_ERROR_DICTIONARY.items()}
--- transaction/process_payment.py
+++ transaction/process_payment.py
@@ -16,16 +16,21 @@ async def process_payment_with_footprint(account_id, amount, vendor, footprint, 
         timestamp_ = timestamp.generate_timestamp()
         cash_transaction_id = asyncio.create_task(general_handlers.add_cash_transaction(account_id, amount, timestamp, vendor))
         footprint_account_id = asyncio.create_task(general_handlers.get_associated_footprint_account_id(account_id))
+
         await cash_transaction_id
-        await footprint_account_id
+        try:
+            await footprint_account_id
+        except general_handler_error.NoAssociatedFootprintAccountError:
+            await cash_transaction_id
+            return cash_transaction_id
     except general_handler_error.GeneralHandlerError as e:
         return str(e)
     else:
         # TODO: change input_method to dictionary in input_footprint
         # TODO: return status code, and call a blackbox payment function
         try:
-            footprint_transaction_id = await general_handlers.add_footprint_transaction(footprint_account_id, footprint, timestamp_, cash_transaction_id, 0, confidence)
+            await general_handlers.add_footprint_transaction(footprint_account_id, footprint, timestamp_, cash_transaction_id, 0, confidence)
         except general_handler_error.GeneralHandlerError as e:
             return str(e)
-        else:
+        finally:
             return cash_transaction_id
