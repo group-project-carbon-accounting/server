ea1d4d0882ccff65e26da73249e687c8fcad92e2
Riya Patel <rpp28@cam.ac.uk>
2022-02-19 13:38:48 +0000

    Request Handlers

--- main.py
+++ main.py
@@ -4,20 +4,38 @@ from tornado.web import Application
 from tornado_sqlalchemy import SQLAlchemy
 from tornado import httpclient
 import tornado
+import json
+import urllib
+import process_payment
+import general.account
+import general.user
 
-class MainHandler(tornado.web.RequestHandler):
+class AddTransactionHandler(tornado.web.RequestHandler) :
     def get(self):
-        self.write("Hello World!")
+        transaction = process_payment(self.request.body)
+        self.write(transaction)
+
+class CreateUserHandler(tornado.web.RequestHandler) :
+    def get(self):
+        data = json.loads(self.request.body)
+        general.user(self, data['username'], data['email'])
+
+class CreateAccountHandler(tornado.web.RequestHandler) :
+    def get(self):
+        data = json.loads(self.request.body)
+        general.account(self, data['user'], data['account_type'])
+
+
 
 db = SQLAlchemy("postgresql://postgres:postgres@localhost:5432/db")
 
 
 app = Application ([
-    (r'/test', MainHandler), 
+    #(r'/test', MainHandler), 
     (r'/user/create', CreateUserHandler), 
     (r'/account/create', CreateAccountHandler), 
     (r'/transaction/add', AddTransactionHandler),
-    (r'/transaction/update', UpdateTransactionHAndler)
+    # (r'/transaction/update', UpdateTransactionHAndler)
 ])
 
 if __name__ == '__main__':
