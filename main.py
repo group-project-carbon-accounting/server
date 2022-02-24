046a279bc6be305aa2975f36095ee6b020d00f80
Riya Patel <rpp28@cam.ac.uk>
2022-02-24 20:17:51 +0000

    Added condition for negative carbon cost

--- main.py
+++ main.py
@@ -26,16 +26,21 @@ class AddTransactionHandler(tornado.web.RequestHandler) :
         response = await httpclient.AsyncHTTPClient().fetch("http://localhost:8888/purchase/add", method = 'POST', body = self.request.body)
         data = json.loads(self.request.body)
         carbon = 0
+        offset = 0
         if (data['item_list'] and not data['carbon_cost']) :
             for key in data['item_list'] :
                 response1 = await httpclient.AsyncHTTPClient().fetch("http://localhost:8889/product/get/" + str(key['comp_id']) + "/" + str(key['prod_id']), method = 'GET')
                 details = json.loads(response1.body)
                 carbon += details['carbon_cost']
         elif (data['carbon_cost']):
-            carbon += data['carbon_cost']
+            if (data['carbon_cost'] >= 0) :
+                carbon += data['carbon_cost']
+            else :
+                offset += data['carbon_cost']
         entityResponse = await httpclient.AsyncHTTPClient().fetch("http://localhost:8889/entity/get/" + str(data['buyr_id']), method = 'GET')
         entityDetails = json.loads(entityResponse.body)
         entityDetails['carbon_cost'] += carbon
+        entityDetails['carbon_offset'] += offset
         newEntityResponse = await httpclient.AsyncHTTPClient().fetch("http://localhost:8888/entity/update", method = 'POST', body = json.dumps(entityDetails))
         self.write(response.body)
 
