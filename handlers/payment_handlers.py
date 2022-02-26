a763b09f5cbbb01e76cb3aa451c101e584843f15
Riya Patel <rpp28@cam.ac.uk>
2022-02-26 13:25:11 +0000

    Payment handlers complete

--- handlers/payment_handlers.py
+++ handlers/payment_handlers.py
@@ -1,35 +1,28 @@
 import tornado.web
 from handlers.async_fetch import async_fetch, GET, POST
+import json
+from tornado import httpclient
 
 class PaymentProcessHandler(tornado.web.RequestHandler):
     async def post(self):
-        pass
-
-'''
-class TransactionAddHandler(tornado.web.RequestHandler) :
-    async def post(self):
         request_data = json.loads(self.request.body)
-
-
-
-
-        response_data = await async_fetch('/purchase/add', POST, self.request.body)
+        response_data = await async_fetch('/purchase/add', POST, request_data)
         carbon = 0
         offset = 0
-        if response_data['item_list'] and not data['carbon_cost'] :
-            for key in data['item_list'] :
-                response1 = await httpclient.AsyncHTTPClient().fetch("http://localhost:8889/product/get/" + str(key['comp_id']) + "/" + str(key['prod_id']), method = 'GET')
-                details = json.loads(response1.body)
-                carbon += details['carbon_cost']
-        elif (data['carbon_cost']):
-            if (data['carbon_cost'] >= 0) :
-                carbon += data['carbon_cost']
+        if (request_data['item_list'] and request_data['carbon_cost'] == 0) :
+            for key in request_data['item_list'] :
+                product_response = await async_fetch("/product/get/" + str(key['comp_id']) + "/" + str(key['prod_id']), GET)
+                carbon += product_response['carbon_cost']
+        else:
+            if (request_data['carbon_cost'] >= 0) :
+                carbon += request_data['carbon_cost']
             else :
-                offset += data['carbon_cost']
-        entityResponse = await httpclient.AsyncHTTPClient().fetch("http://localhost:8889/entity/get/" + str(data['buyr_id']), method = 'GET')
-        entityDetails = json.loads(entityResponse.body)
-        entityDetails['carbon_cost'] += carbon
-        entityDetails['carbon_offset'] += offset
-        newEntityResponse = await httpclient.AsyncHTTPClient().fetch("http://localhost:8888/entity/update", method = 'POST', body = json.dumps(entityDetails))
-        self.write(response.body)
-'''
+                offset += request_data['carbon_cost']
+        
+        entityResponse = await async_fetch("/entity/get/" + str(request_data['buyr_id']), method = 'GET')
+        entityResponse['carbon_cost'] += carbon
+        entityResponse['carbon_offset'] += offset
+        newEntityResponse = await async_fetch("/entity/update", POST, entityResponse)
+        request_data['carbon_cost'] = carbon
+        update_response = await async_fetch('/purchase/update', POST, request_data)
+        self.write(json.dumps(update_response))
